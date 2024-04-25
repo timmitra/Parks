ed2fedde7219a26d90b6ae343dec38f27de0a9cc
Tim Mitra <tim@it-guy.com>
2024-04-25 17:05:11 -0400

    filter with Predicates

--- Parks/ContentView.swift
+++ Parks/ContentView.swift
@@ -15,7 +15,21 @@ struct ContentView: View {
   //@Query(sort: \ParkModel.name, order: .reverse) private var parks: [ParkModel]
   
   // sort on multiple
-  @Query(sort: [SortDescriptor(\ParkModel.country), SortDescriptor(\ParkModel.rating, order: .reverse)]) private var parks: [ParkModel]
+  //@Query(sort: [SortDescriptor(\ParkModel.country), SortDescriptor(\ParkModel.rating, order: .reverse)]) private var parks: [ParkModel]
+  
+  /* @Query(filter: #Predicate<ParkModel>{ park in
+    park.country == "Canada"
+  }) private var parks: [ParkModel] */
+
+ /* @Query<ParkModel>(filter: #Predicate { park in
+    park.country == "Canada"
+  }) private var parks: [ParkModel] */
+
+  @Query<ParkModel>(filter: #Predicate { park in
+    (park.country == "United States" && park.region == "Utah")
+    || park.country == "Italy"
+    
+  }) private var parks: [ParkModel]
 
     var body: some View {
         List(parks) { park in
