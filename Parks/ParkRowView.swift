7e365de75ede7d854d98c8c76806be6da284bcf4
Tim Mitra <tim@it-guy.com>
2024-04-25 16:47:13 -0400

    parkRowView

--- Parks/ParkRowView.swift
+++ Parks/ParkRowView.swift
@@ -6,13 +6,40 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ParkRowView: View {
+  let park: ParkModel
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      HStack {
+        Image(uiImage: park.viewImage)
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .frame(height: 60.0)
+          .clipShape(.rect(cornerRadius: 8))
+        VStack {
+          Text(park.name)
+            .font(.title)
+          Text(park.viewLocation)
+            .fontWeight(.light)
+        }
+        Spacer()
+      }
     }
 }
 
 #Preview {
-    ParkRowView()
+  let container = try! ModelContainer(for: ParkModel.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+  
+  let park = ParkModel(
+    name: "Arches",
+    image: UIImage(resource: .arches).pngData()!,
+    region: "Utah",
+    country: "United States",
+    rating: 6
+  )
+  container.mainContext.insert(park)
+  
+  return ParkRowView(park: park)
 }
