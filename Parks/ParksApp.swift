f06f380fc3b0a3cd15067d8bbb464f160cf662dd
Tim Mitra <tim@it-guy.com>
2024-04-25 16:15:34 -0400

    Initial Commit

---
+++ Parks/ParksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParksApp.swift
+//  Parks
+//
+//  Created by Tim Mitra on 2024-04-25.
+//
+
+import SwiftUI
+
+@main
+struct ParksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
